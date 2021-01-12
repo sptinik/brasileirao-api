c44f36f4b7fe3cd22ae5d80f750cfe2ac565e0f5
Your Name <you@example.com>
2021-01-12 19:20:59 -0300

    Conectando JSoup no google

--- src/main/java/br/com/ecf/util/ScrapingUtil.java
+++ src/main/java/br/com/ecf/util/ScrapingUtil.java
@@ -1,17 +1,43 @@
 package br.com.ecf.util;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.ecf.dto.PartidaGoogleDTO;
 
 public class ScrapingUtil {
 	
+	private static final Logger LOOGER = LoggerFactory.getLogger(ScrapingUtil.class);
 	private static final String BASE_URL_GOOGLE = "https://www.google.com/search?q=";
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String url = BASE_URL_GOOGLE + "palmeiras+x+corinthas+08/08/2020" + COMPLEMENTO_URL_GOOGLE;
 		
-		PartidaGoogleDTO partida = new PartidaGoogleDTO()
-		partida.getGolsEquipeCasa()
+		ScrapingUtil scraping = new ScrapingUtil();
+		scraping.obtemInformacoesPartida(url);
+	}
+	
+	public PartidaGoogleDTO obtemInformacoesPartida(String url) {
+		PartidaGoogleDTO partida = new PartidaGoogleDTO();
+		
+		Document document = null;
+		
+		try {
+			document = Jsoup.connect(url).get();
+			
+			String title = document.title();
+			LOOGER.info("Titulo da pagina: {}", title);
+		} catch (IOException e) {
+			LOOGER.error("ERROR AO TENTAR CONECTAR NO GOOGLE COM JSOUP -> {}", e.getMessage());
+			e.printStackTrace();
+		}		
+		
+		return partida;
 	}
 
 }
